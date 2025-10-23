7eee273f5974ee5f7bb8dc6aec39e70114cabe57
Amir Law <amair084@evit.edu>
2025-10-23 14:32:35 -0700

    add tons of data manipulations buttons, redid a bit of gui, added logo, and resized the app.

--- app.py
+++ app.py
@@ -1,20 +1,208 @@
 #NOCODESQL main app file
 import customtkinter as ctk
 import sqlite3
-from tkinter import PhotoImage
 import os
+from tkinter import messagebox
+from PIL import Image
+
+data_dir = os.path.dirname(os.path.abspath(__file__))
+data_folder = os.path.join(data_dir, "data")
+
 
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
+ctk.set_default_color_theme("green")
 
 class App(ctk.CTk):
+
+
     def __init__(self):
         super().__init__()
         self.title("NoCodeSQL")
-        self.geometry("850x575")
+        self.geometry("850x975")
         self.configure(bg="#0F0F0F")
         self.resizable(False, False)
         self.iconbitmap("resources/logo.ico")
+
+        self.DB_FILE = os.path.join(data_folder, "")
+
+        self.mainframe = ctk.CTkFrame(self, width=800, height=900)
+        self.mainframe.pack(padx=20, pady=30, fill="both", expand=True)
+
+        long_logo = ctk.CTkImage(
+            light_image=Image.open("resources/titlelogo.png"),
+            dark_image=Image.open("resources/titlelogo.png"),
+            size=(80,18)
+        )
+
+
+        self.manipulation_table = ctk.CTkFrame(self.mainframe, width=775, height=335)
+        self.manipulation_table.pack(padx=0, pady=0, fill="both", expand=True)
+        self.manipulation_table.place(x=20,y=5)
+
+        logo_label = ctk.CTkLabel(self, image=long_logo, text="")
+
+        logo_label.image = long_logo  # keep reference
+        logo_label.place(x=750, y=0)  # adjust positioning
+
+        self.loadbutton = ctk.CTkButton(self.mainframe, text="Create/Load Table", command=self.createTable)
+        self.loadbutton.pack(pady=10)
+
+        self.table_name_entry = ctk.CTkEntry(self.mainframe, width=100, height=20, font=("Helvetica", 14))
+        self.table_name_entry.pack(pady=0)
+
+
+        self.tableframe = ctk.CTkFrame(self.mainframe, width=775, height=550)
+        self.tableframe.place(x=20,y=370)
+
+        self.table_title = ctk.CTkButton(self, text="Run Query", state="normal", height=40, width=160)
+        self.table_title.place(x=345,y=300)
+
+        #self.table_title.bind("<Button-1>", self.block_click())
+
+        self.addcolumnbtn = ctk.CTkButton(self.manipulation_table, text="Add Column", command=self.pickselect)
+        self.removecolumnbtn = ctk.CTkButton(self.manipulation_table, text="Remove Column", command=self.pickfilter)
+        self.editcolumnbtn = ctk.CTkButton(self.manipulation_table, text="Edit Column", command=self.pickjoin)
+
+        self.addrowbtn = ctk.CTkButton(self.manipulation_table, text="Add Row", command=self.pickselect)
+        self.removerowbtn = ctk.CTkButton(self.manipulation_table, text="Remove Row", command=self.pickfilter)
+        self.editrowbtn = ctk.CTkButton(self.manipulation_table, text="Edit Row", command=self.pickjoin)
+
+        self.selectbtn = ctk.CTkButton(self.manipulation_table, text="Select", command=self.pickselect)
+        self.filterbtn = ctk.CTkButton(self.manipulation_table, text="Where", command=self.pickfilter)
+        self.joinbtn = ctk.CTkButton(self.manipulation_table, text="Join", command=self.pickjoin)
+
+        self.orderbybtn = ctk.CTkButton(self.manipulation_table, text="Order By", command=self.pickselect)
+        self.groupbybtn = ctk.CTkButton(self.manipulation_table, text="Group By", command=self.pickfilter)
+        self.aggregatebtn = ctk.CTkButton(self.manipulation_table, text="Aggregate", command=self.pickjoin)
+
+        self.loadnew= ctk.CTkButton(self.manipulation_table, text="Load New", command=self.pickselect)
+        self.exportbtn = ctk.CTkButton(self.manipulation_table, text="Export", command=self.pickfilter)
+        self.clearqrybtn = ctk.CTkButton(self.manipulation_table, text="Clear Query", command=self.pickjoin)
+
+        self.all_manipulation_buttons = {self.addcolumnbtn, self.removecolumnbtn, self.editcolumnbtn,
+                                         self.addrowbtn, self.removerowbtn, self.editrowbtn, self.selectbtn,
+                                         self.filterbtn, self.joinbtn, self.orderbybtn, self.groupbybtn,
+                                         self.aggregatebtn, self.loadnew, self.exportbtn, self.clearqrybtn
+                                         }
+
+
+        logo_label.lift()
+
+        ## SQL Functions
+
+    def pickselect(self):
+        for x in self.all_manipulation_buttons:
+            x.place_forget()
+        self.selectbtn.place(x=315,y=10)
+
+    def pickjoin(self):
+        for x in self.all_manipulation_buttons:
+            x.place_forget()
+        self.joinbtn.place(x=315,y=10)
+
+    def pickfilter(self):
+        for x in self.all_manipulation_buttons:
+            x.place_forget()
+        self.filterbtn.place(x=315,y=10)
+
+
+    def manipulateTable(self):
+        self.loadbutton.forget()
+        self.table_name_entry.forget()
+
+        self.loadnew.place(x=615,y=15)
+        self.exportbtn.place(x=615,y=50)
+        self.clearqrybtn.place(x=615,y=85)
+
+        self.orderbybtn.place(x=465, y=15)
+        self.groupbybtn.place(x=465, y=50)
+        self.aggregatebtn.place(x=465, y=85)
+
+        self.selectbtn.place(x=315, y=15)
+        self.filterbtn.place(x=315, y=50)
+        self.joinbtn.place(x=315, y=85)
+
+        self.addrowbtn.place(x=165, y=15)
+        self.removerowbtn.place(x=165, y=50)
+        self.editrowbtn.place(x=165, y=85)
+
+        self.addcolumnbtn.place(x=15, y=15)
+        self.removecolumnbtn.place(x=15, y=50)
+        self.editcolumnbtn.place(x=15, y=85)
+
+
+    def block_click(self):
+        return "break"
+
+    def loadTable(self):
+       #self.table_title.configure(text=(f"Table: {self.DB_FILE[50::]}"))
+
+       self.title(f"Table: {self.DB_FILE[50::]}")
+
+       # Clear previous widgets
+       for widget in self.tableframe.winfo_children():
+           widget.destroy()
+
+       if not os.path.exists(self.DB_FILE):
+           return
+
+       conn = sqlite3.connect(self.DB_FILE)
+       cursor = conn.cursor()
+
+       try:
+           cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+           table_name = cursor.fetchone()
+           if table_name is None:
+               conn.close()
+               return
+           table_name = table_name[0]
+
+           cursor.execute(f"PRAGMA table_info({table_name})")
+           columns_info = cursor.fetchall()
+           column_names = [col[1] for col in columns_info]
+
+           cursor.execute(f"SELECT * FROM {table_name}")
+           rows = cursor.fetchall()
+       except sqlite3.OperationalError:
+           conn.close()
+           return
+
+       conn.close()
+
+       for col, text in enumerate(column_names):
+           label = ctk.CTkLabel(self.tableframe, text=text, font=("Helvetica", 12, "bold"))
+           label.grid(row=0, column=col, padx=2, pady=0)
+
+       for r, row in enumerate(rows, start=1):
+           for c, value in enumerate(row):
+               label = ctk.CTkLabel(self.tableframe, text=str(value))
+               label.grid(row=r, column=c, padx=20, pady=0)
+
+       self.manipulateTable()
+
+    def createTable(self):
+        table_name = self.table_name_entry.get()
+
+        if  table_name.strip() == "":
+            messagebox.showwarning("Input Error", "Table Name cannot be empty!")
+            return
+        self.DB_FILE = os.path.join(data_folder, table_name + ".db")
+
+        conn = sqlite3.connect(self.DB_FILE)
+        cursor = conn.cursor()
+
+        cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS generic_table (
+            column1 TEXT
+        )
+        """)
+
+
+        conn.commit()
+        conn.close()
+
+        self.loadTable()
+
 
 if __name__ == "__main__":
     app = App()
